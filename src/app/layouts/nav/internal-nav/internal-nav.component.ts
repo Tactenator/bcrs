60612a2cf072f17308165eb9d8d616b4667e932b
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-11-21 10:57:05 -0500

    Update internal-nav.component.ts

--- src/app/layouts/nav/internal-nav/internal-nav.component.ts
+++ src/app/layouts/nav/internal-nav/internal-nav.component.ts
@@ -7,6 +7,8 @@
 // imports statements
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-internal-nav',
@@ -15,7 +17,15 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class InternalNavComponent {
   @ViewChild('sidenav') sidenav!: MatSidenav;
+
+  constructor(private router: Router, private cookieService: CookieService) {}
+
   openSidenav() {
     this.sidenav.open();
+  }
+
+  signOut() {
+    this.cookieService.deleteAll();
+    this.router.navigate(['/']);
   }
 }
