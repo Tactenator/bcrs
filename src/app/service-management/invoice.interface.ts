23ddba4ad1cd1b5a1ce1cfeabf6652008dd8048e
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-11-21 16:12:09 -0500

    files created

---
+++ src/app/service-management/invoice.interface.ts
@@ -0,0 +1,6 @@
+export class InvoiceModel {
+    _id?: string;
+    username: string;
+    userId: string;
+    services : Array<any>;
+}
---
+++ src/app/service-management/services/service.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: service.component.ts
+ * Author: Patrick Cuauro
+ * Date: 11/20/2023
+ * Description: Service component (service page)
+ *  this component will be used to display the service page
+ *
+ */
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-service',
+    templateUrl: './service.component.html',
+    styleUrls: ['./service.component.css']
+    })
+export class ServiceComponent implements OnInit {
+    constructor() { }
+    ngOnInit() {
+    }
+}
