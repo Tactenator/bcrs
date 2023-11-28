41d115909e9149799591a50abfaf6f7db83ac373
Patrick Cuauro <pcuauronava@my365.bellevue.edu>
2023-11-28 13:02:56 -0500

    added table

---
+++ src/app/user-management/user-table/user-table-datasource.ts
@@ -0,0 +1,148 @@
+import { UserTableService } from './../user-table.service';
+import { DataSource } from '@angular/cdk/collections';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { map } from 'rxjs/operators';
+import { Observable, of as observableOf, merge } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from 'src/app/models/user';
+//service created to retrieve data from the server (see user.service.ts)
+//new service created to retrieve data from the server
+
+export interface UserTableItem
+  extends Pick<User, 'userId' | 'email' | 'firstName' | 'lastName'> {}
+
+// TODO: replace this with real data from your application
+const USER_DATA: UserTableItem[] = [
+  {
+    userId: '1980',
+    firstName: 'Patrick',
+    lastName: 'Cuauro',
+    email: 'pcuauronava@my365.bellevue.edu',
+  },
+  {
+    userId: '1987',
+    firstName: 'Liseth',
+    lastName: 'Avila',
+    email: 'lisethavila@home.com',
+  },
+  {
+    userId: '2013',
+    firstName: 'Paul',
+    lastName: 'Cuauro',
+    email: 'pauldcuauro@home.com',
+  },
+  {
+    userId: '2019',
+    firstName: 'Rebecca',
+    lastName: 'Cuauro',
+    email: 'rebeccacuauro@home.com',
+  },
+  {
+    userId: '2021',
+    firstName: 'Samantha',
+    lastName: 'Cuauro',
+    email: 'samanthacuauro@home.com',
+  },
+];
+
+// const TEST_DATA: UserTableItem = UserTableService.findUsers();
+// ^?
+
+/**
+ * Data source for the UserTable view. This class should
+ * encapsulate all logic for fetching and manipulating the displayed data
+ * (including sorting, pagination, and filtering).
+ */
+export class UserTableDataSource extends DataSource<UserTableItem> {
+  data: Observable<UserTableItem[]>;
+  paginator: MatPaginator | undefined;
+  sort: MatSort | undefined;
+
+  constructor(private userTableService: UserTableService) {
+    super();
+    this.data = this.userTableService.findUsers() as any as Observable<
+      UserTableItem[]
+    >;
+  }
+
+  /**
+   * Connect this data source to the table. The table will only update when
+   * the returned stream emits new items.
+   * @returns A stream of the items to be rendered.
+   */
+  connect(): Observable<UserTableItem[]> {
+    // if (this.paginator && this.sort) {
+    //   // Combine everything that affects the rendered data into one update
+    //   // stream for the data-table to consume.
+    //   return merge(
+    //     observableOf(this.data),
+    //     this.paginator.page,
+    //     this.sort.sortChange
+    //   ).pipe(
+    //     map(() => {
+    //       return this.getPagedData(this.getSortedData([...this.data]));
+    //     })
+    //   );
+    // } else {
+    //   throw Error(
+    //     'Please set the paginator and sort on the data source before connecting.'
+    //   );
+    // }
+    return this.data;
+  }
+
+  /**
+   *  Called when the table is being destroyed. Use this function, to clean up
+   * any open connections or free any held resources that were set up during connect.
+   */
+  disconnect(): void {}
+
+  /**
+   * Paginate the data (client-side). If you're using server-side pagination,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getPagedData(data: UserTableItem[]): UserTableItem[] {
+    if (this.paginator) {
+      const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
+      return data.splice(startIndex, this.paginator.pageSize);
+    } else {
+      return data;
+    }
+  }
+
+  /**
+   * Sort the data (client-side). If you're using server-side sorting,
+   * this would be replaced by requesting the appropriate data from the server.
+   */
+  private getSortedData(data: UserTableItem[]): UserTableItem[] {
+    if (!this.sort || !this.sort.active || this.sort.direction === '') {
+      return data;
+    }
+
+    return data.sort((a, b) => {
+      const isAsc = this.sort?.direction === 'asc';
+      switch (this.sort?.active) {
+        case 'userId':
+          return compare(+a.userId, +b.userId, isAsc);
+        case 'firstName':
+          return compare(+a.firstName, +b.firstName, isAsc);
+        case 'lastName':
+          return compare(+a.lastName, +b.lastName, isAsc);
+        case 'email':
+          return compare(+a.email, +b.email, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+/** Simple sort comparator for example ID/Name columns (for client-side sorting). */
+function compare(
+  a: string | number,
+  b: string | number,
+  isAsc: boolean
+): number {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+}
