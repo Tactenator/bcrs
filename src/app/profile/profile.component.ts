85e3b260afde0d1029155ef296f79479b1c030bb
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-12-08 21:20:33 -0500

    reyes-clean up profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,15 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { COOKIE_KEYS } from '../constants/cookie-keys';
 import { User } from '../models/user';
-import { UserService } from '../user-configuration/user.service';
 import { SecurityService } from '../security/security.service';
-import { ProfileService } from './profile.service';
-
-export interface Profile
-  extends Pick<
-    User,
-    'firstName' | 'lastName' | 'email' | 'address' | 'phoneNumber' | 'role'
-  > {}
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -18,28 +11,15 @@ export interface Profile
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  userId: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  address: string;
-  phoneNumber: string;
   role: string;
-  test: any;
-  currentUser$ = this.securityService.currentUser$;
+  currentUser$: Observable<User> = this.securityService.currentUser$;
 
   constructor(
     private cookieService: CookieService,
-    public profileService: ProfileService,
     public securityService: SecurityService
   ) {}
 
   ngOnInit(): void {
-    this.userId = this.cookieService.get(COOKIE_KEYS.USER_ID);
-    console.log(this.userId);
-    this.test = this.profileService.getUser(this.userId);
-    console.log(this.test);
-    this.firstName = this.cookieService.get(COOKIE_KEYS.NAME);
     this.role = this.cookieService.get(COOKIE_KEYS.ROLE);
     // this.address = this.profileService.identifier;
   }
